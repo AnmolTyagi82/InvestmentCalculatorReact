5d07451913ef3bbc4db76ed10e74be0eff960cf3
AnmolTyagi82 <anmoltyagi849@gmail.com>
2024-01-13 20:44:59 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/AnmolTyagi82/InvestmentCalculatorReact",
+  base: "/InvestmentCalculatorReact/",
   plugins: [react()],
 })
